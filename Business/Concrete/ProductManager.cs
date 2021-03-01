1bfbb75eba319d3f76e315867639b5dece301be2
b-tekinli <btbtbt664@gmail.com>
2021-03-01 05:56:08 +0300

    updated

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -38,32 +38,32 @@ namespace Business.Concrete
         {
             // İş Kodları
             // Yetkisi var mı?
-            if (DateTime.Now.Hour == 22)
+
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
+        }
+
+        public IDataResult<List<Product>> GetAllByCategoryId(int id)
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.CategoryId == id));
+        }
+
+        public IDataResult<Product> GetById(int productId)
+        {
+            return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
+        }
+
+        public IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max)
+        {
+            return new SuccessDataResult<List<Product>>(_productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max));
+        }
+
+        public IDataResult<List<ProductDetailDto>> GetProductDetails()
+        {
+            if (DateTime.Now.Hour == 23)
             {
-                return new ErrorDataResult();
+                return new ErrorDataResult<List<ProductDetailDto>>(Messages.MaintenanceTime);
             }
-
-            return new SuccessDataResult<List<Product>>(_productDal.GetAll(), true, "Ürünler listelendi.");
-        }
-
-        public List<Product> GetAllByCategoryId(int id)
-        {
-            return _productDal.GetAll(p => p.CategoryId == id);
-        }
-
-        public Product GetById(int productId)
-        {
-            return _productDal.Get(p => p.ProductId == productId);
-        }
-
-        public List<Product> GetByUnitPrice(decimal min, decimal max)
-        {
-            return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
-        }
-
-        public List<ProductDetailDto> GetProductDetails()
-        {
-            return _productDal.GetProductDetails();
+            return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
         }
     }
 }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             // Data Transformation Objects
-            ProductTest();
+            // ProductTest();
             // IoC
         }
 
@@ -26,7 +26,21 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+
+            if (result.Success == true)
+            {
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + " / " + product.CategoryName);
+                }
+            }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+
+            foreach (var product in productManager.GetProductDetails().Data)
             {
                 Console.WriteLine(product.ProductName + " / " + product.CategoryName);
             }
